4ccd5aaaa61195b0626e748f8d48a6077b01842a
Anton Sapyanov <anton.sapyanoff@gmail.com>
2015-07-13 17:07:27 +0300

    Update User model

--- application/models/user.py
+++ application/models/user.py
@@ -5,12 +5,12 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(64))
-    mobile_phone = db.Column(db.String(15))
-    inner_phone = db.Column(db.String(4))
-    email = db.Column(db.String)
-    birth_date = db.Column(db.Date)
-    avatar = db.Column(db.String)
+    full_name = db.Column(db.String(64))
+    mobile_phone = db.Column(db.String(15), nullable=True)
+    inner_phone = db.Column(db.String(4), nullable=True)
+    email = db.Column(db.String, unique=True)
+    birth_date = db.Column(db.Date, nullable=True)
+    avatar = db.Column(db.String, nullable=True)
 
     def __repr__(self):
         return "<User {login}>".format(login=self.login)
