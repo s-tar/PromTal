90588ef1b86b7e4862e4470fe919bae332216c9d
Anton Sapyanov <anton.sapyanoff@gmail.com>
2015-07-13 17:50:44 +0300

    Update User model (removed restrictions on some fields)

--- application/models/user.py
+++ application/models/user.py
@@ -3,13 +3,13 @@ from application import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # TODO check, if autoincrement
     login = db.Column(db.String(64), unique=True)
     full_name = db.Column(db.String(64))
-    mobile_phone = db.Column(db.String(15), nullable=True)
-    inner_phone = db.Column(db.String(4), nullable=True)
-    email = db.Column(db.String, unique=True)
-    birth_date = db.Column(db.Date, nullable=True)
+    mobile_phone = db.Column(db.String, nullable=True)  # TODO Add constraint on length and format
+    inner_phone = db.Column(db.String, nullable=True)   # TODO Add constraint on length and format
+    email = db.Column(db.String, unique=True)  # TODO Add constraint on length; can't be nullable in future
+    birth_date = db.Column(db.Date, nullable=True)  # TODO Add default value
     avatar = db.Column(db.String, nullable=True)
 
     def __repr__(self):
