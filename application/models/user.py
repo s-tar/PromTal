ea63b8ec5418a6b85cb031b31e7369dadffd1115
Cyril Degtyarenko <k.degtyarenko@gmail.com>
2015-07-14 17:54:38 +0300

    added test migration

--- application/models/user.py
+++ application/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)  # TODO check, if autoincrement
     login = db.Column(db.String(64), unique=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), default=1)
     full_name = db.Column(db.String(64))
     mobile_phone = db.Column(db.String, nullable=True)  # TODO Add constraint on length and format
     inner_phone = db.Column(db.String, nullable=True)   # TODO Add constraint on length and format
