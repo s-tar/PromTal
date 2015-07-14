1a29f132b7bab1e79f8e60d6927c965b7b9694e9
Mihail Selezniov <mihailselezniov@yandex.ru>
2015-07-14 14:47:08 +0300

    Add login restore

--- application/views/main.py
+++ application/views/main.py
@@ -20,6 +20,14 @@ def session_check(text=None):
 def profile():
     return render_template('profile/profile.html')
 
-@app.route("/pro")
+@app.route("/main")
 def pro():
-    return render_template('index2.html')
+    return render_template('main.html')
+
+@app.route("/login")
+def login():
+    return render_template('login/login.html')
+
+@app.route("/restore")
+def restore():
+    return render_template('login/restore.html')
