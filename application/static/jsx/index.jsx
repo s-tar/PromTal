297fcc2a4727b0c9db871dbc5797d1846ef68e7f
Mihail Selezniov <mihailselezniov@yandex.ru>
2015-07-15 16:49:44 +0300

    Add login.jsx in login

--- application/static/jsx/index.jsx
+++ application/static/jsx/index.jsx
@@ -1,17 +1,4 @@
 React.render(
-    <AJAXForm action="/user/login" method="post" className="form-signin" onSuccess={wellDone}>
-        <Input className="form-control" type="text" name="login" placeholder="Логин" onChange={someChange}/>
-        <Input className="form-control" type="password" name="password" placeholder="Пароль" />
-        <Input className="btn btn-lg btn-primary btn-block" type="submit" value="Вход"/>
-        <label className="checkbox pull-left">
-            <Input type="checkbox" value="remember-me"/>
-            Remember me
-        </label>
-    </AJAXForm>,
-    document.getElementById('log_in_form'))
-
-
-React.render(
     <AJAXForm action="/user/login" method="post" onSuccess={wellDone}>
         <Input type="text" name="login" placeholder="Логин" onChange={someChange}/>
         <Input type="password" name="password" placeholder="Пароль" />
