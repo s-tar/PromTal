41e9efac400d5dacf8a26e7c1b4a40d041d2ae9e
Cyril Degtyarenko <k.degtyarenko@gmail.com>
2015-07-31 16:30:46 +0300

    added user role serializer for API

---
+++ application/models/serializers/user.py
@@ -0,0 +1,18 @@
+__author__ = 'kdegtyarenko'
+
+from marshmallow import Schema, fields
+
+
+class UserSchema(Schema):
+
+    id = fields.Int()
+    email = fields.Email()
+    full_name = fields.Str()
+    login = fields.Str()
+    mobile_phone = fields.Str()
+    inner_phone = fields.Str()
+    birth_date = fields.Date()
+    avatar = fields.Str()
+    photo = fields.Str()
+    photo_s = fields.Str()
+    skype = fields.Str()
