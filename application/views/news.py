5861fa77d5bde251335cc93b7e55492f2c5dc501
s-tar <srg.trn.mail@gmail.com>
2015-08-04 11:09:48 +0300

    Minimal permissions added

--- application/views/news.py
+++ application/views/news.py
@@ -3,7 +3,7 @@ from application.models.news import News
 from application.utils import auth
 from application.utils.validator import Validator
 from application.views.main import main
-from flask import render_template, request
+from flask import render_template, request, abort
 from flask.json import jsonify
 
 module = Module('news', __name__, url_prefix='/news')
@@ -25,6 +25,8 @@ def news_one(id):
 @module.get('/edit/<int:id>')
 def news_form(id=None):
     news = News.get(id) or News()
+    if id and news.author != auth.service.get_user():
+        abort(403)
     return render_template('news/form.html', **{'news': news})
 
 @module.route("/save", methods=['POST'])
@@ -35,10 +37,12 @@ def save():
     v.field('text').required()
     user = auth.service.get_user()
     if not user.is_authorized():
-        v.add_error('title', 'Сорян, паца. Нет пользователя - нет новости.')
+        abort(403)
     if v.is_valid():
         data = v.valid_data
         news = News.get(data.id) or News()
+        if news.author and news.author != user:
+            abort(403)
         news.title = data.title
         news.text = data.text
         news.author = user
